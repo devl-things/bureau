6bbd5736be381139b1f8ce049f08ee0c8996b414
lbaracic <69803758+lbaracic@users.noreply.github.com>
2025-12-16 23:55:24 +0100

    #83 - Added more problem codes

--- server/src/Bureau.Primitives/Errors/ProblemCodes.cs
+++ server/src/Bureau.Primitives/Errors/ProblemCodes.cs
@@ -2,13 +2,27 @@
 {
     public static class ProblemCodes
     {
+        public static class Operation
+        {
+            public const string Failed = "operation.failed";
+        }
+        public static class Request
+        {
+            public const string InvalidId = "request.invalid_id";
+            public const string IdMismatch = "request.id_mismatch";
+        }
+        public static class Resource
+        {
+            public const string NotFound = "resource.not_found";
+            public const string Conflict = "resource.conflict";
+        }
         public static class System
         {
             public const string UnexpectedError = "system.unexpected_error";
         }
-        public static class Operation
+        public static class Validation
         {
-            public const string Failed = "operation.failed";
+            public const string Failed = "validation.failed";
         }
     }
 }
