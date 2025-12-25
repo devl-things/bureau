0c03b43e150e75e27143dbf3492c4c17f4632f80
lbaracic <69803758+lbaracic@users.noreply.github.com>
2025-12-25 10:04:27 +0100

    #115 - made a test page to test Bureau.Admin.Frame wiring - added config for admin app registry

--- server/src/Niles.Chores.Web/Program.cs
+++ server/src/Niles.Chores.Web/Program.cs
@@ -1,3 +1,4 @@
+using Bureau.Admin.Hosting.Configurations;
 using Microsoft.Extensions.Options;
 using Niles.Chores.Web.Configurations;
 
@@ -8,15 +9,21 @@ namespace Niles.Chores.Web
         public static async Task Main(string[] args)
         {
             WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            builder.WebHost.UseStaticWebAssets();
 
             builder.Services.Configure<AppConfiguration>(builder.Configuration);
 
+            builder.Services.AddBureauAdminAppsRegistry(builder.Configuration);
+            builder.Services.AddRazorPages();
 
             var app = builder.Build();
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
 
+            app.UseRouting();
+
+            app.MapRazorPages();
             app.MapGet("/config", (IOptions<AppConfiguration> config) =>
             {
                 return Results.Json(config.Value);
