f3f10e4f4a5695414403bf55bf0978b4f86a5646
lbaracic <69803758+lbaracic@users.noreply.github.com>
2026-01-14 10:52:17 +0100

    #69 - Introduce IHealthProbe interface for custom health checks. - Add ProbeHealthCheck<TProbe> and HealthChecksBuilderExtensions to enable easy registration of probes with ASP.NET Core health checks. - Clean up <Build> tags in solution file.

---
+++ server/src/Bureau.AspNetCore/HealthChecks/ProbeHealthCheck.cs
@@ -0,0 +1,20 @@
+﻿using Bureau.Primitives;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Bureau.AspNetCore.HealthChecks
+{
+    public sealed class ProbeHealthCheck<TProbe> : IHealthCheck where TProbe : class, IHealthProbe
+    {
+        private readonly TProbe _probe;
+
+        public ProbeHealthCheck(TProbe probe)
+        {
+            _probe = probe;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            return await _probe.IsHealthyAsync(cancellationToken) ? HealthCheckResult.Healthy() : HealthCheckResult.Unhealthy();
+        }
+    }
+}
